d3bb38bd4a6f82d33e4537c15773790668518612
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-20 20:11:31 +0530

    ADDED: use local storage and server url attribute

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,4 +1,6 @@
 let site_properties = {
+    use_local_storage: "true",
+    server_url: "http://localhost:3000/ContactDB/",
     home_page: "../pages/home.html",
     add_contact_page: "../pages/addressbook_form.html"
 }; 

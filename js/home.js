aa4fa7ce4edae0919298f115f5a0f85978d9df03
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-20 13:02:56 +0530

    REFACTORED: table header is displayed dynamically using template literals

--- js/home.js
+++ js/home.js
@@ -3,29 +3,22 @@ window.addEventListener('DOMContentLoaded',(event) => {
 });
 
 const createInnerHtml = () => {
-    const innerHtml = `
-    <tr>
-        <th>FullName</th>
-        <th>Address</th>
-        <th>City</th>
-        <th>State</th>
-        <th>Zip code</th>
-        <th>Phone Number</th>
-    </tr>
-    <tr>
-        <td>Arpitha Bhat</td>
-        <td>carstreet tth</td>
-        <td>Bengaluru</td>
-        <td>Karnataka</td>
-        <td>563298</td>
-        <td>+918987654345</td>
-        <td>
-            <img id="1" onclick="remove(this)" alt="delete"
-            src="../assets/icons/delete-black-18dp.svg" style="padding-right: 5px;">
-            <img id="1" alt="edit" onclick="update(this)"
-            src="../assets//icons/create-black-18dp.svg">
-        </td>
-    </tr>
+    const headerHtml = "<tr><th>FullName</th><th>Address</th><th>City</th><th>State</th><th>Zip code</th><th>Phone Number</th></tr>";
+    const innerHtml = `${headerHtml}
+        <tr>
+            <td>Arpitha Bhat</td>
+            <td>carstreet tth</td>
+            <td>Bengaluru</td>
+            <td>Karnataka</td>
+            <td>563298</td>
+            <td>+918987654345</td>
+            <td>
+                <img id="1" onclick="remove(this)" alt="delete"
+                src="../assets/icons/delete-black-18dp.svg" style="padding-right: 5px;">
+                <img id="1" alt="edit" onclick="update(this)"
+                src="../assets//icons/create-black-18dp.svg">
+            </td>
+        </tr>
     `;
     document.querySelector('#table-display').innerHTML = innerHtml;
 } 

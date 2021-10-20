c7b279f16cfef54ceb484a8bf01a2ee6c4e78c9c
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-20 20:33:05 +0530

    REFACTORED: regex patterns to validate phone,zip and address

--- js/utility.js
+++ js/utility.js
@@ -4,16 +4,16 @@ const checkName = (name) => {
 }
 
 const checkPhone = (phone) => {
-    let phoneRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    let phoneRegex = RegExp("^([\\+][1-9][0-9]|[1-9][0-9])?[0-9]{10}$");
     if(!phoneRegex.test(phone)) throw 'Phone Number is Invalid!';
 }
 
 const checkAddress = (address) => {
-    let addressRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    let addressRegex = RegExp('^[a-zA-Z0-9]{3,}([\\s]?[a-zA-Z0-9]{3,})*$');
     if(!addressRegex.test(address)) throw 'Address is Incorrect!';
 }
 
 const checkZip = (zip) => {
-    let zipRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    let zipRegex = RegExp("^[0-9]{3}[\\s]?[0-9]{3}$");
     if(!zipRegex.test(zip)) throw 'Zipcode is Incorrect!';
 }

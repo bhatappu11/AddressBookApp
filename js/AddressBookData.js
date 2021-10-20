5cc3c5e25f8d95901ec3e68d9bfbeea7bc25c3db
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-20 10:50:14 +0530

    REFACTORED: regex pattern for validating address

--- js/AddressBookData.js
+++ js/AddressBookData.js
@@ -21,7 +21,7 @@ class AddressBookData {
     }
     get address() { return this._address}
     set address(address){
-        let addressRegex = RegExp('[a-zA-Z]{3,}([\\s]*[a-zA-Z]{3,})*');
+        let addressRegex = RegExp('^[a-zA-Z0-9]{3,}([\\s]?[a-zA-Z0-9]{3,})*$');
         if(addressRegex.test(address))
             this._address = address;
         else throw 'Address is Incorrect';

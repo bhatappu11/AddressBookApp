b0e3653fd1df2c37b6ecbe902f396cd82f381023
Arpitha Bhat <arpithabht12@gmail.com>
2021-10-20 14:58:54 +0530

    ADDED: function to remove contact from person details table

--- js/home.js
+++ js/home.js
@@ -23,9 +23,9 @@ const createInnerHtml = () => {
             <td>${contactData._zipcode}</td>
             <td>${contactData._phone}</td>
             <td>
-                <img name="${contactData._id}" onclick="remove(this)" alt="delete"
+                <img id="${contactData._id}" onclick="remove(this)" alt="delete"
                 src="../assets/icons/delete-black-18dp.svg" style="padding-right: 5px;">
-                <img name="${contactData._id}" alt="edit" onclick="update(this)"
+                <img id="${contactData._id}" alt="edit" onclick="update(this)"
                 src="../assets//icons/create-black-18dp.svg">
             </td>
         </tr>
@@ -33,3 +33,14 @@ const createInnerHtml = () => {
     document.querySelector('#table-display').innerHTML = innerHtml;
     }
 } 
+const remove = (node) => {
+    let contactData = contactDataList.find(contact => contact._id == node.id);
+    if(!contactData) return;
+    const index = contactDataList
+                    .map(contact => contact._id)
+                    .indexOf(contactData._id);
+    contactDataList.splice(index, 1);
+    localStorage.setItem("AddressBookList",JSON.stringify(contactDataList));
+    document.querySelector(".address-count").textContent = contactDataList.length;
+    createInnerHtml();
+}
